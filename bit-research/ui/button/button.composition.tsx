3bbd3c218c390629e8a251188486f6ffd687e5e1
Jonas Ivy Imperial <jonasivyimperial@gmail.com>
2022-12-05 11:31:13 +0800

    Initial Commit

---
+++ bit-research/ui/button/button.composition.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button } from './button';
+import { Span } from '@tff_jonasivy/bit-research.ui.span';
+
+export const BasicButton = () => {
+  return (
+    <Button>Hello World!</Button>
+  );
+}
---
+++ bit-research/ui/span/span.composition.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Span } from './span';
+
+export const BasicSpan = () => {
+  return (
+    <Span>hello world!</Span>
+  );
+}
